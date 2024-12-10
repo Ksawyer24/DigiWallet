218f84a1376c0c3d03bcaeaffd461bcd095ce714
Kay <sawyercage1212@gmail.com>
2024-12-10 15:49:25 +0000

    !

--- DigiWallet/Repos/Interface/IWalletRepo.cs
+++ DigiWallet/Repos/Interface/IWalletRepo.cs
@@ -5,8 +5,7 @@ namespace DigiWallet.Repos.Interface
 {
     public interface IWalletRepo
     {
-        Task<WalletDto> GetWalletByUserIdAsync(Guid userId);
-        Task<Wallet> CreateWalletAsync(Wallet wallet);
+        Task<Wallet> CreateWalletForUserAsync(Guid userId);
         Task AddFundsAsync(Guid userId, decimal amount);
         Task DeductFundsAsync(Guid walletId, decimal amount);
         Task<List<TransactionDto>> GetTransactionsByWalletIdAsync(Guid walletId);
--- DigiWallet/Repos/WalletRepo.cs
+++ DigiWallet/Repos/WalletRepo.cs
@@ -2,6 +2,7 @@
 using DigiWallet.Dto;
 using DigiWallet.Models;
 using DigiWallet.Repos.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace DigiWallet.Repos
 {
@@ -13,27 +14,52 @@ namespace DigiWallet.Repos
         {
             this.digitalDbContext = digitalDbContext;
         }
+
         public async Task AddFundsAsync(Guid userId, decimal amount)
         {
-            throw new NotImplementedException();
+            // Begin a transaction
+            using var transaction = await digitalDbContext.Database.BeginTransactionAsync();
+
+            try
+            {
+                // Retrieve the wallet
+                var wallet = await digitalDbContext.Wallets
+                 .FirstOrDefaultAsync(w => w.UserId == userId);
+
+                if (wallet == null)
+                {
+                    throw new Exception("Wallet not found for the given user.");
+                }
+
+                // Update the wallet balance
+                wallet.Balance += amount;
+
+                // Save changes to the wallet
+                await digitalDbContext.SaveChangesAsync();
+
+                // Commit the transaction
+                await transaction.CommitAsync();
+            }
+
+            catch (Exception)
+            {
+                // Rollback transaction in case of failure
+                await transaction.RollbackAsync();
+                throw;
+            }
         }
 
-        public Task<Wallet> CreateWalletAsync(Wallet wallet)
+        public Task<Wallet> CreateWalletForUserAsync(Guid userId)
         {
             throw new NotImplementedException();
         }
 
-        public async Task DeductFundsAsync(Guid walletId, decimal amount)
+        public Task DeductFundsAsync(Guid walletId, decimal amount)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<List<TransactionDto>> GetTransactionsByWalletIdAsync(Guid walletId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<WalletDto> GetWalletByUserIdAsync(Guid userId)
+        public Task<List<TransactionDto>> GetTransactionsByWalletIdAsync(Guid walletId)
         {
             throw new NotImplementedException();
         }
